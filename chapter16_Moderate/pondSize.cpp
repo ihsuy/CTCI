36877c1503447a2f446079690bc00f87102e670c
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-01 15:20:30 +0900

    quick update

---
+++ chapter16_Moderate/pondSize.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+#include <list>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <math.h>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <utility>
+#include <bitset>
+#include <fstream>
+#include <string>
+
+typedef long long ll;
+inline int two(int n) { return 1 << n; }
+inline int test(int n, int b) { return (n>>b)&1; }
+inline void set_bit(int & n, int b) { n |= two(b); }
+inline void unset_bit(int & n, int b) { n &= ~two(b); }
+inline int last_bit(int n) { return n & (-n); }
+inline int ones(int n) { int res = 0; while(n && ++res) n-=n&(-n); return res; }
+template<typename T>
+inline void inspect(T& t){typename T::iterator i1 = t.begin(), i2 = t.end(); while (i1 != i2) {std::cout << (*i1) << ' '; i1++;} std::cout << '\n';}
+
+/////////////////////////////////////////////////////////////
+using namespace std;
+
+/*
+Pond Sizes: You have an integer matrix representing a plot of land,
+where the value at that loca- tion represents the height above sea level.
+A value of zero indicates water. A pond is a region of water connected vertically, 
+horizontally, or diagonally. The size of the pond is the total number of connected 
+water cells. Write a method to compute the sizes of all ponds in the matrix.
+*/
+
+
+
+int main()
+{
+
+
+	return 0;
+}
