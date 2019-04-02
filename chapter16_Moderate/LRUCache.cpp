53ddb1e40a599055deef962db94a4526f143e1e7
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-02 15:34:04 +0900

    quick update

--- chapter16_Moderate/LRUCache.cpp
+++ chapter16_Moderate/LRUCache.cpp
@@ -31,7 +31,11 @@ inline void inspect(T& t){typename T::iterator i1 = t.begin(), i2 = t.end(); whi
 using namespace std;
 
 /*
-
+LRU Cache: Design and build a "least recently used" cache, 
+which evicts the least recently used item. The cache should map 
+from keys to values (allowing you to insert and retrieve a value 
+associated with a particular key) and be initialized with a max size. 
+When it is full, it should evict the least recently used item.
 */
 
 
