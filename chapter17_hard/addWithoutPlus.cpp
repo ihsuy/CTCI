422a08ca290144b94d19e31c76a454ef8ae4832e
Yushi Qiu <jooehisaishi@gmail.com>
2019-04-05 16:29:17 +0900

    quick update

--- chapter17_hard/addWithoutPlus.cpp
+++ chapter17_hard/addWithoutPlus.cpp
@@ -35,11 +35,53 @@ Add Without Plus: Write a function that adds two numbers.
 You should not use + or any arithmetic operators.
 */
 
+int bitmanip_add(const int& a, const int& b)
+{
+	int mask = 1;
+	int loc = 0;
+	int result = 0;
+	int carry = 0;
 
+	int larger = ((a>b)?a:b);
+	while(mask <= larger)
+	{	// get every bit
+		int ba = ((a&mask) != 0);
+		int bb = ((b&mask) != 0);
+
+		// add bits and carry
+		// note there are 2 cases for ba == bb
+		// namely 0 0 and 1 1, both of them add up to 0
+		// and there are 2 cases for ba != bb
+		// namely 0 1 and 1 0, both of them add up to 1
+		// this is equivalent to XOR
+		// then we add carry using the same method
+		int sum = (ba^bb)^carry;
+
+		// carry is 0 if (ba + bb + previous_carry)>1
+		// this is true when at least two of the 3 numbers are 1
+		// we check if thats true or false
+		carry = (ba&bb)|((ba|bb)&carry);
+
+		// add bit to result
+		result |= (sum<<loc); 
+
+		mask = mask<<1;
+
+		loc++;
+	}
+	if(carry != 0)
+	{	// deal with the case when last carry isn't 0
+		result |= (carry<<loc);
+	}
+	
+	return result;
+}
 
 int main()
 {
-
+	int a = 777;
+	int b = 222; 
+	cout << bitmanip_add(a, b) << '\n';
 
 	return 0;
 }
